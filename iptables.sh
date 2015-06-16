979b3d26a6b0b07157c2e29e3a187590342ba5cb
arkam <ArKam@users.noreply.github.com>
2015-06-16 14:00:34 +0200

    adding POI Port output ACCEPT.

--- iptables.sh
+++ iptables.sh
@@ -28,9 +28,10 @@ iptables -t filter -A OUTPUT -p icmp -j ACCEPT
 iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp --dport 22 -j ACCEPT
 
-# Serveur web (HTTP)
+# Serveur web (HTTP+POI)
 iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp --dport 8080 -j ACCEPT
 
 # Serveur web (HTTPS)
 iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT
